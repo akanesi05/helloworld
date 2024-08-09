407cc3f71a3d8b825d287c526bc1f095bb0a40de
mitsuiakane <akane.sumitomo@gmail.com>
2024-08-09 18:32:58 +0900

    顔のぼかし続き

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,4 +2,9 @@ class Board < ApplicationRecord
   # mount_uploader :image, ImageUploader
   mount_uploader :board_image, ImageUploader
   mount_uploader :board_image, ImageUploader
+ mount_uploader :image, ImageUploader
+ def image_presence
+  errors.add(:image, "をアップロードしてください") unless image.present?
+  end
+
 end
